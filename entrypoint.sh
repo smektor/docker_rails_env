489b933f2e305cb13a05cdb61018f55f2128fb92
smektor <wiktor.jankowski13@gmail.com>
2020-10-27 20:53:52 +0100

    add yarn install in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,5 @@ rm -f /sportnacja/tmp/pids/server.pid
 # Check for new gems if there are install them
 bundle check || bundle install
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
+yarn install --check-files
 exec "$@"
